fea924d53304424b0ab2af6ee2232d95213aecdb
Yousef Farge <yousef.farge@gmail.com>
2024-11-28 08:32:38 -0600

    test: added integration test for health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use email_newsletter::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address.");
+    run(listener)?.await
 }
